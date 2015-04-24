0ac153a854619e93170ac53f6a079bebb8643146
jhowcs <jhowcs@gmail.com>
2015-04-23 23:57:32 -0300

    Sobrecarga do método quebrarTexto para aceitar apenas o parâmetro texto e informando por default que o texto será quebrado a cada 45 caracteres. Ajuste no método quebrar texto para adicionar hífen(-) ao quebrar uma palavra. EX: "Celular" com a quantidade máxima de caracteres por linha igual à 2. ce- lu- la- r

--- Main/src/br/com/jonathan/funcoes/Funcoes.java
+++ Main/src/br/com/jonathan/funcoes/Funcoes.java
@@ -1,46 +1,82 @@
 package br.com.jonathan.funcoes;
 
-public class Funcoes {
+import javax.management.AttributeNotFoundException;
 
+public class Funcoes {
 	/**
-	 * Método responsável por quebrar uma String em várias linhas com o máximo de caracteres especificados no parâmetro.
+	 * Método responsável por quebrar uma String em várias linhas.
 	 *
-	 * @param texto        - Texto ser quebrado em várias linhas
-	 * @param maxCharLinha - Quantidade máxima de caracteres por linha. Default 45
+	 * @param texto
+	 *            - Texto á ser quebrado em várias linhas a quantidade máxima de caracteres por linha default é 45.
+	 * @return StringBuilder com o texto quebrado em várias linhas
+	 * 
+	 * @since 23/04/2015
+	 */
+	public static StringBuilder quebrarTexto(String texto) throws AttributeNotFoundException {
+		return quebrarTexto(texto, 45);
+	}
+	
+	/**
+	 * Método responsável por quebrar uma String em várias linhas com o máximo
+	 * de caracteres especificados no parâmetro.
+	 *
+	 * @param texto
+	 *            - Texto á ser quebrado em várias linhas
+	 * @param maxCharLinha
+	 *            - Quantidade máxima, deve ser maior ou igual a 2
 	 * @return StringBuilder com o texto quebrado em várias linhas
 	 * 
 	 * @since 19/04/2015
 	 */
-	public static StringBuilder quebrarTexto(String texto, int maxCharLinha) {
-		StringBuilder sbTexto = new StringBuilder(); 
-	
-		if(texto != null && texto.length() > 0) {						
-			//Remove caracteres em branco duplicados
+	public static StringBuilder quebrarTexto(String texto, int maxCharLinha) throws AttributeNotFoundException {
+		if(maxCharLinha < 2)
+			throw new AttributeNotFoundException("O parâmetro maxCharLinha deve ser maior ou igual a 2!!!");
+		
+		StringBuilder sbTexto = new StringBuilder();
+
+		if (texto != null && texto.length() > 0) {
+			// Remove caracteres em branco duplicados
 			final String str = texto.replaceAll("\\s+", " ").trim();
-			
-			//Array com todas as palavras encontratas no parametro texto
-			final String[] arrayStr   = str.split("\\s");
-			
-			//Se a quantidade máxima de caracteres por linha informada
-			//for 0 então o método assume 45 como default
-			maxCharLinha = maxCharLinha > 0 ? maxCharLinha : 45;
+
+			// Array com todas as palavras encontratas no parametro texto
+			final String[] arrayStr = str.split("\\s");
+
 			int contChar = 0;
-			
-			for (int i = 0; i < arrayStr.length; i++) {	
-				contChar+= arrayStr[i].length();
-				
-				if(contChar < maxCharLinha) {
+
+			for (int i = 0; i < arrayStr.length; i++) {
+				contChar += arrayStr[i].length();
+
+				if (contChar <= maxCharLinha) {
 					sbTexto.append(arrayStr[i]);
 				} else {
-					contChar = arrayStr[i].length();					
-					sbTexto.append("\n");
-					sbTexto.append(arrayStr[i]);					
+					contChar = arrayStr[i].length();
+
+					if (contChar > maxCharLinha) {
+						int iniCopia = 0;			
+						while (iniCopia < (contChar - (maxCharLinha-1))) {
+							sbTexto.append("\n");
+												
+							sbTexto.append(arrayStr[i].substring(iniCopia, iniCopia + (maxCharLinha-1)));
+						
+							
+							iniCopia += maxCharLinha - 1;
+							
+							if(iniCopia < (contChar))
+								sbTexto.append("-");
+						}
+						sbTexto.append("\n");
+						sbTexto.append(arrayStr[i].substring(iniCopia, contChar));
+					} else {
+
+						sbTexto.append("\n");
+						sbTexto.append(arrayStr[i]);
+					}
 				}
 				sbTexto.append(" ");
-				contChar+= 1;
+				contChar += 1;
 			}
 		}
-		
+
 		return sbTexto;
 	}
 }
